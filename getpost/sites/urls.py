35f35308fbb9fd8e643185922c76660a4081c7c8
ferc255 <slutskiyad@gmail.com>
2017-10-03 19:21:49 +0400

    I can work with input json

--- getpost/sites/urls.py
+++ getpost/sites/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from sites import views
 
 urlpatterns = [
-    url(r'^(?P<id>\d+)?$', views.get_request, name='main_view'),
+    url(r'^(?P<id>\d+)?$', views.main_view, name='main_view'),
 ]
--- getpost/sites/views.py
+++ getpost/sites/views.py
@@ -3,6 +3,20 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-def get_request(request, id):
-    return JsonResponse({'foo': 'bar'})
+@csrf_exempt
+def main_view(request, id):
+    if request.method == 'GET':
+        if id:
+            return JsonResponse({'type': 'GET', 'id': 'available',})
+        else:
+            return JsonResponse({'type': 'GET', 'id': 'absent'})
+    elif request.method == 'POST' and not id:
+        target_url = json.loads(request.body)['url']
+        print(target_url)
+
+
+        response = 'goodmorning'
+        return JsonResponse({'type': 'POST', 'id': 'absent', 'response': response})
