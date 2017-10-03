dc589ab2d3d8d8c8e3f1e07b6e667daac00e1fb7
ferc255 <slutskiyad@gmail.com>
2017-10-03 17:05:07 +0400

    Initial commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/aleksey/development/django/getpost/env/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ setPaths.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. ./env/bin/activate
+export DJANGO_SETTINGS_MODULE=getpost.settings
+export PYTHONPATH=/home/aleksey/development/django/getpost/getpost
